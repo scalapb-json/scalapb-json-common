86177c02199af4246daae86fa8a18f073d0cd735
xuwei-k <6b656e6a69@gmail.com>
2019-12-18 11:46:18 +0900

    Update scalafmt

--- core/js/src/main/scala/com/google/protobuf/TextFormat.scala
+++ core/js/src/main/scala/com/google/protobuf/TextFormat.scala
@@ -1,6 +1,7 @@
 package com.google.protobuf
 
 object TextFormat {
+
   /**
    * [[https://github.com/google/protobuf/blob/v3.5.1/java/core/src/main/java/com/google/protobuf/TextFormat.java#L1111-L1136]]
    */
