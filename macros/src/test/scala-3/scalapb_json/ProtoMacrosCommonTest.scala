a92e2508760aea8290fbdb5aa15fc64ab8446d1d
xuwei-k <6b656e6a69@gmail.com>
2022-03-13 15:39:10 +0900

    tweak ProtoMacrosCommonTest

--- macros/src/test/scala-3/scalapb_json/ProtoMacrosCommonTest.scala
+++ macros/src/test/scala-3/scalapb_json/ProtoMacrosCommonTest.scala
@@ -18,6 +18,28 @@ import scala.quoted.staging.withQuotes
 
 object ProtoMacrosCommonTest extends Scalaprops {
 
+  private[this] implicit val value1: Gen[Value] = {
+    val primitive: Gen[Value] = Gen
+      .oneOf(
+        Gen.value(Value.Kind.Empty),
+        Gen.value(Value.Kind.NullValue(NullValue.NULL_VALUE)),
+        Gen[Int].map(a => Value.Kind.NullValue(NullValue.fromValue(a))),
+        Gen[Double].map(x => if (x.isNaN) Value.Kind.NumberValue(0) else Value.Kind.NumberValue(x)),
+        Gen.alphaNumString.map(Value.Kind.StringValue.apply),
+        Gen[Boolean].map(Value.Kind.BoolValue.apply),
+      )
+      .map(Value.apply(_))
+
+    val list1: Gen[ListValue] = Gen.listOf(primitive).map(ListValue.apply(_))
+    val struct1: Gen[Struct] = Gen.mapGen(Gen.alphaNumString, primitive).map(Struct.apply(_))
+
+    Gen.oneOf(
+      primitive,
+      list1.map(a => Value(Value.Kind.ListValue(a))),
+      struct1.map(a => Value(Value.Kind.StructValue(a))),
+    )
+  }
+
   val test: Property = {
     given Compiler = Compiler.make(getClass.getClassLoader)
     val x1: Quotes => Property = run(testImpl)
@@ -25,38 +47,15 @@ object ProtoMacrosCommonTest extends Scalaprops {
     withQuotes(x2)
   }
 
-  private[this] def testImpl(using Quotes): Expr[Quotes => Property] =
-    '{
-      val primitive: Gen[Value] = Gen
-        .oneOf(
-          Gen.value(Value.Kind.Empty),
-          Gen.value(Value.Kind.NullValue(NullValue.NULL_VALUE)),
-          Gen[Int].map(a => Value.Kind.NullValue(NullValue.fromValue(a))),
-          Gen[Double]
-            .map(x => if (x.isNaN) Value.Kind.NumberValue(0) else Value.Kind.NumberValue(x)),
-          Gen.alphaNumString.map(Value.Kind.StringValue.apply),
-          Gen[Boolean].map(Value.Kind.BoolValue.apply),
-        )
-        .map(Value.apply(_))
-
-      val list1: Gen[ListValue] = Gen.listOf(primitive).map(ListValue.apply(_))
-      val struct1: Gen[Struct] = Gen.mapGen(Gen.alphaNumString, primitive).map(Struct.apply(_))
-
-      implicit val value1: Gen[Value] =
-        Gen.oneOf(
-          primitive,
-          list1.map(a => Value(Value.Kind.ListValue(a))),
-          struct1.map(a => Value(Value.Kind.StructValue(a))),
-        )
-
-      { implicit q: Quotes =>
-        {
-          Property.forAll { (x1: Value) =>
-            val x2 = summon[FromExpr[Value]].unapply(Expr(x1))
-            assert(Some(x1) == x2, s"$x1 != $x2")
-            true
-          }
+  private[this] def testImpl(using Quotes): Expr[Quotes => Property] = '{
+    { implicit q: Quotes =>
+      {
+        Property.forAll { (x1: Value) =>
+          val x2 = summon[FromExpr[Value]].unapply(Expr(x1))
+          assert(Some(x1) == x2, s"$x1 != $x2")
+          true
         }
       }
     }
+  }
 }
