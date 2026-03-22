0fb3d01bbc77becf61f55bbdd0c8e29a0d54b7e1
xuwei-k <6b656e6a69@gmail.com>
2026-03-22 14:45:52 +0900

    Update JsonFormatException

--- core/shared/src/main/scala/scalapb_json/JsonFormatException.scala
+++ core/shared/src/main/scala/scalapb_json/JsonFormatException.scala
@@ -1,5 +1,5 @@
 package scalapb_json
 
-case class JsonFormatException(msg: String, cause: Exception) extends Exception(msg, cause) {
+case class JsonFormatException(msg: String, cause: Throwable) extends Exception(msg, cause) {
   def this(msg: String) = this(msg, null)
 }
